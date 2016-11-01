659c0e6acf086ee4a82fc8a76e2b757e4ae8343d
Oscar Martinez <oscar_noa@hotmail.com>
2016-11-01 22:48:10 +0100

    Initial commit

---
+++ app/src/main/java/com/example/reno/comunicacionactividades/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.reno.comunicacionactividades;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
