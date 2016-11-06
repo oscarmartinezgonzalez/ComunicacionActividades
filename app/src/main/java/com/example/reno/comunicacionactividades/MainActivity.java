fc4cff7dbeb4aa844b6b9ea02b41a5f7fe1664c3
Oscar Martinez <oscar_noa@hotmail.com>
2016-11-06 18:53:30 +0100

    Added intent

--- app/src/main/java/com/example/reno/comunicacionactividades/MainActivity.java
+++ app/src/main/java/com/example/reno/comunicacionactividades/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,12 +12,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Bundle extras = getIntent().getExtras();
+        String s = extras.getString("resultado");
+
     }
 
     public void lanzaIntent(View view){
         Intent i = new Intent(this, NewActivity.class);
         i.putExtra("usuario", "Pepito Perez");
         i.putExtra("edad", 27);
+        EditText et1 = (EditText)findViewById(R.id.editText);
+        System.out.println(et1.getText());
+        i.putExtra("texto", et1.getText().toString());
         startActivity(i);
     }
 }
--- app/src/main/java/com/example/reno/comunicacionactividades/NewActivity.java
+++ app/src/main/java/com/example/reno/comunicacionactividades/NewActivity.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 /**
  * Created by Reno on 02/11/2016.
@@ -17,13 +19,24 @@ public class NewActivity extends Activity {
         Bundle extras = getIntent().getExtras();
         String s = extras.getString("usuario");
         int i = extras.getInt("edad");
+        String texto = extras.getString("texto");
+        TextView tv2 = (TextView)findViewById(R.id.textView2);
+        tv2.setText("Hola "+ texto);
     }
 
     public void lanzaIntent(View view){
         finish();
         Intent i = new Intent(this, MainActivity.class);
-        i.putExtra("usuario", "Pepito Perez");
         i.putExtra("edad", 27);
+        i.putExtra("resultado", "Aceptado!");
+        startActivity(i);
+    }
+
+    public void lanzaIntent2(View view){
+        finish();
+        Intent i = new Intent(this, MainActivity.class);
+        i.putExtra("edad", 27);
+        i.putExtra("resultado", "Rechazado!");
         startActivity(i);
     }
 
